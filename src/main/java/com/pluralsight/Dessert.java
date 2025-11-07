d80906fb365b780d4ed9d7e5fe2d8fba66fe9f14
Vincentacheampong <Vincentacheampong10@gmail.com>
2025-11-07 14:46:57 -0500

    case 3 added

--- src/main/java/com/pluralsight/Dessert.java
+++ src/main/java/com/pluralsight/Dessert.java
@@ -14,10 +14,13 @@ public class Dessert {
     public static Dessert createFromUserInput(Scanner scanner) {
         System.out.println("Choose dessert (1.Cake 8, 2.Chin Chin 6, 3.Fruit Cup 7, 4.None 0): ");
         switch (scanner.nextLine()) {
+            case "1": return new Dessert("Cake", 8);
             case "2": return new Dessert("Chin Chin", 6);
-            case "3": return new Dessert("Fruit Cup", 7);
-            default: return new Dessert("Cake Slice", 8);
+            case "3": return new Dessert("ruit Cup", 7);
+            default:
+                System.out.println("Choose an option above");
         }
+        return null;
     }
 
     public double getPrice() { return price; }
